b4dc7d5e5b6304af13d4b8dfc01acb45121057be
unknown <gpereira383@Gmail.com>
2024-03-01 22:25:37 -0300

    feat: calculator added

--- src/main/java/br/edu/senaisp/servlet/OlaMundo.java
+++ src/main/java/br/edu/senaisp/servlet/OlaMundo.java
@@ -8,21 +8,27 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/olamundo")
+@WebServlet(urlPatterns = "/calc")
 public class OlaMundo extends HttpServlet {
-	
+
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		String somOuSub = req.getParameter("p1");
-		int v1 = Integer.parseInt(req.getParameter("p2"));
-		int v2 = Integer.parseInt(req.getParameter("p3"));
-		
-		if(somOuSub.equals("soma")) {
-			resp.getWriter().append(("<html><h1>"+v1+"+"+v2+"="+(v1 + v2)+"</h1></html>"));
-		}if (somOuSub.equals("subtracao")) {
-			resp.getWriter().append(("<html><h1>"+v1+"+"+v2+"="+"</h1></html>"));
-	}
-	
+		try {
+			String operacao = req.getParameter("oper");
+			int v1 = Integer.parseInt(req.getParameter("val1"));
+			int v2 = Integer.parseInt(req.getParameter("val2"));
+
+			if (operacao.equalsIgnoreCase("soma")) {
+				resp.getWriter().append(("<html><h1>" + v1 + "+" + v2 + "=" + (v1 + v2) + "</h1></html>"));
+			} else if (operacao.equalsIgnoreCase("subtrair")) {
+				resp.getWriter().append(("<html><h1>" + v1 + "-" + v2 + "=" + (v1 - v2) + "</h1></html>"));
+			} else if (operacao.equalsIgnoreCase("multip")) {
+				resp.getWriter().append(("<html><h1>" + v1 + "x" + v2 + "=" + (v1 * v2) + "</h1></html>"));
+			} else if (operacao.equalsIgnoreCase("divi")) {
+				resp.getWriter().append(("<html><h1>" + v1 + "/" + v2 + "=" + (v1 / v2) + "</h1></html>"));
+			}
+		} catch (Exception e) {
+			resp.getWriter().append("<html><h1>ERRO: " + e + "</h1></html>");
+		}
 	}
 }
---
+++ src/main/java/br/edu/senaisp/util/Eoperacao.java
@@ -0,0 +1,6 @@
+package br.edu.senaisp.util;
+
+public enum Eoperacao {
+	
+	SOMA,SUBTRACAO,MULTIPLICACAO,DIVISAO
+}
